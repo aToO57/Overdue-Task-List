0076dd84cd4e4a31ab79db31080e03653dd854f9
aTo <aTo@ato_mac.home>
2016-04-20 11:31:36 +0200

    Initial Commit

---
+++ Overdue Task List/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Overdue Task List
+//
+//  Created by aTo on 20/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Overdue Task List/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Overdue Task List
+//
+//  Created by aTo on 20/04/2016.
+//  Copyright © 2016 aTo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
